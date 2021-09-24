c12f26a40c86e76596eef9ab31f506014696543a
jocyber <jordaneharman@protonmail.com>
2021-09-24 14:33:47 -0400

    added own way of printing the bits in lfsr

--- algorithms/lfsr.cpp
+++ algorithms/lfsr.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <stdlib.h>
 #include <bitset>
+#include <string.h>
 
-//algorithm for LFSR register
+//algorithm for LFSR
+std::string bits(int val, int numBits);
+
 int main(int argc, char *argv[])
 {
 	unsigned int state = 0b10110;
+	std::cout << bits(state, 5) << '\n';
 
-	for(int i = 1; i < 32; i++) {
-		std::bitset<5> x(state);
-		std::cout << x << '\n';
-
+	for(int i = 1; i < 16; i++) {
 		state >>= 1;
 		unsigned int lastbit = state & 1;
 		unsigned int secondlastbit = (state >> 1) & 1;
@@ -19,7 +20,22 @@ int main(int argc, char *argv[])
 		result <<= 4;
 
 		state |= result;
+		std::cout << bits(state, 5) << '\n';
 	}
 
 	return EXIT_SUCCESS;
+}
+
+//prints the bits
+std::string bits(int val, int numBits) {
+	std::string bits = "";
+
+	for(int i = 0; i < numBits; i++) {
+		bool lastbit = (val & 1);
+		bits = std::to_string(lastbit) + bits;
+
+		val >>= 1;
+	}
+
+	return bits;
 }

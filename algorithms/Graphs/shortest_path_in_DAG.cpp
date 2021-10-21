330fc626f367fac12de3f355ce6f9da9b91e624c
jocyber <jordaneharman@protonmail.com>
2021-10-20 20:36:53 -0400

    added shortest path in DAGS. Also added a recursive function to find shortest path based on distances.

--- algorithms/Graphs/shortest_path_in_DAG.cpp
+++ algorithms/Graphs/shortest_path_in_DAG.cpp
@@ -2,32 +2,76 @@
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
+#include <utility>
+#include <list>
+
+using std::pair;
+using std::make_pair;
+
+using p = std::pair<int,int>;
 
 class Digraph {
 
-    std::unordered_map<int, std::vector<int>> adj_list;
+    std::unordered_map<int, std::vector<p>> adj_list; // edge, weight
 	 int numNodes = 0;
 
 public:
-    Digraph(){}
+    Digraph(){} // default constructor
 
-    void addEdge(const int vertex, const int vertex2) {
+    void addEdge(const int vertex, const int vertex2, const int weight) {
         if(adj_list.find(vertex) != adj_list.end() && adj_list.find(vertex2) != adj_list.end()) {
-            for(const int &x : adj_list[vertex]) {
-					if(x == vertex2)
+            for(const auto &x : adj_list[vertex]) {
+					if(x.first == vertex2)
 						return;
 				}
 
-            adj_list[vertex].push_back(vertex2);
+            adj_list[vertex].push_back(make_pair(vertex2, weight));
         }
     }
 
     void addVertex(const int node) {
         if(adj_list.find(node) == adj_list.end()) {
-            adj_list[node] = std::vector<int>{};
-				numNodes++;
+            adj_list[node] = std::vector<p>{};
+			numNodes++;
 		  }
     }
+
+	//////////////////////////////////////////////
+	//shortest path member function
+	//////////////////////////////////////////////
+
+	std::list<int> shortestPathDAG(const int &source, const int &dest) { //O(V + E)
+		std::vector<int> top_sort = topological_sort(); // O(V + E)
+		std::unordered_map<int,int> dist;
+		std::unordered_map<int,int> prev;
+
+		dist[top_sort[0]] = 0; // set initial node distance to 0
+
+		for(int i = 0; i < top_sort.size(); ++i) {
+			int node = top_sort[i];
+
+			for(const auto &x : adj_list[node]) {
+				if(dist.find(x.first) == dist.end()) {
+					dist[x.first] = dist[node] + x.second;
+					prev[x.first] = node;
+				}
+				else {
+					int distance = dist[node] + x.second;
+
+					if(dist[x.first] > distance) {
+						dist[x.first] = distance;
+						prev[x.first] = node;
+					}
+				}
+			}
+		}
+		
+		std::list<int> path;
+		shortestDistance(prev, source, dest, path);
+		return path;
+	}
+	/////////////////////////////////////////////
+	//////////////////////////////////////////////
 
 	 std::vector<int> topological_sort() {
 		std::unordered_set<int> visited;
@@ -47,15 +91,22 @@ private:
 	 void topological_sort(int node, std::unordered_set<int> &visited, std::vector<int> &list, int &index) {
 			visited.insert(node);
 
-			for(const int &x: adj_list[node]) {
-					if(visited.find(x) == visited.end()) {
-						topological_sort(x, visited, list, index);
+			for(const auto &x: adj_list[node]) {
+					if(visited.find(x.first) == visited.end()) {
+						topological_sort(x.first, visited, list, index);
 					}
-
 			}
 
 			list[index] = node;
 			index--;
+	 }
+
+	//find the shortest path between 2 nodes given the shortest distances in DAG
+	void shortestDistance(std::unordered_map<int,int> &prev, int source, int dest, std::list<int> &list) {
+		 if(source != dest)
+		 	shortestDistance(prev, source, prev[dest], list);
+
+		 list.push_back(dest);
 	 }
 };
 
@@ -67,18 +118,17 @@ int main()
 	g.addVertex(2);
 	g.addVertex(3);
 	g.addVertex(4);
-	g.addVertex(5);
 
-	g.addEdge(1,2);
-	g.addEdge(2,3);
-	g.addEdge(3,4);
-	g.addEdge(5,3);
-	g.addEdge(5,4);
+	g.addEdge(1, 2, 2);
+	g.addEdge(1, 3, 3);
+	g.addEdge(2, 3, 2);
+	g.addEdge(2, 4, 3);
+	g.addEdge(1, 4, 7);
 
-	std::vector<int> list = g.topological_sort();
+	std::list<int> path = g.shortestPathDAG(1, 3);
 
-	for(const int &x: list) {
-		std::cout << x << " ";
+	for(auto i = path.begin(); i != path.end(); ++i) {
+		std::cout << *i << ' ';
 	}
 
 	std::cout << std::endl;

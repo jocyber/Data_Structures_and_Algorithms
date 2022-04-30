8ce03bdadfc3768e3f0c7d1c95b53a18d16c475f
jocyber <jordaneharman@protonmail.com>
2022-04-29 21:32:35 -0400

    finished word search

---
+++ algorithms/Word-Search/Word-Search.cpp
@@ -0,0 +1,185 @@
+#include "../../includes/trie.h"
+#include <vector>
+#include <string>
+#include <unordered_set>
+#include <iostream>
+#include <fstream>
+#include <ctype.h>
+
+std::unordered_set<std::string> words;
+std::vector<std::vector<char>> boardRes;
+
+void printBoard(const std::vector<std::vector<char>> &board);
+
+//algorithm to solve the puzzle
+bool searchWord(const std::vector<std::vector<char>> &board, Trie &trie, unsigned int i, unsigned int j, int hor_dir = 0, int vert_dir = 0, std::string word = "") {
+    std::string uWord = word + board[i][j];
+
+    if(words.find(uWord) != words.end()) {
+			boardRes[i][j] = uWord[uWord.length() - 1];
+      return true;
+		}
+    else if(trie.isWordPresent(word)) {
+        if(hor_dir == 0 && vert_dir == 0) { //check all directions
+            if(i > 0 && j > 0) { //upper left corner
+                bool found = searchWord(board, trie, i - 1, j - 1, -1, -1,uWord);
+								if(found) {	
+									boardRes[i][j] = uWord[uWord.length() - 1];
+									return true;
+								}
+						}
+
+            if(i > 0 && j < board[i].size() - 1) { //top right corner
+	              bool found = searchWord(board, trie, i - 1, j + 1, 1, -1,uWord);
+								if(found) {	
+									boardRes[i][j] = uWord[uWord.length() - 1];
+									return true;
+								}
+						}
+
+            if(i < board.size() - 1 && j < board[i].size() - 1) {//bottom right corner
+                bool found = searchWord(board, trie, i + 1, j + 1, 1, 1,uWord);
+								if(found) {	
+									boardRes[i][j] = uWord[uWord.length() - 1];
+									return true;
+								}
+						}
+
+            if(i < board.size() - 1 && j > 0) { //bottom left corner
+                bool found = searchWord(board, trie, i + 1, j - 1, -1, 1,uWord);
+								if(found) {	
+									boardRes[i][j] = uWord[uWord.length() - 1];
+									return true;
+								}
+						}
+
+            if(j > 0) { //check left
+                bool found = searchWord(board, trie, i, j - 1, -1, 0,uWord);
+								if(found) {	
+									boardRes[i][j] = uWord[uWord.length() - 1];
+									return true;
+								}
+						}
+
+            if(i > 0) { //top
+                bool found = searchWord(board, trie, i - 1, j, 0, -1,uWord);
+								if(found) {	
+									boardRes[i][j] = uWord[uWord.length() - 1];
+									return true;
+								}
+						}
+
+            if(j < board[i].size() - 1) { //right 
+                bool found = searchWord(board, trie, i, j + 1, 1, 0,uWord);   
+								if(found) {	
+									boardRes[i][j] = uWord[uWord.length() - 1];
+									return true;
+								}
+						}
+
+            if(i < board.size() - 1) { //bottom
+                bool found = searchWord(board, trie, i + 1, j, 0, 1,uWord);
+								if(found) {	
+									boardRes[i][j] = uWord[uWord.length() - 1];
+									return true;
+								}
+						}
+        }
+        else {
+						bool found = false;
+
+            if(hor_dir == 1 && vert_dir == 1 && i < board.size() - 1 && j < board[i].size() - 1) // bottom right
+                found = searchWord(board, trie, i + 1, j + 1, 1, 1,uWord);
+            else if(hor_dir == 1 && vert_dir == 0 && j < board[i].size() - 1) // right
+                found = searchWord(board, trie, i, j + 1, 1, 0,uWord); 
+            else if(hor_dir == 0 && vert_dir == 1 && i < board.size() - 1) //bottom
+                found = searchWord(board, trie, i + 1, j, 0, 1,uWord);
+            else if(hor_dir == -1 && vert_dir == 0 && j > 0) //left
+                found = searchWord(board, trie, i, j - 1, -1, 0,uWord);
+            else if(hor_dir == 0 && vert_dir == -1 && i > 0) //top
+                found = searchWord(board, trie, i - 1, j, 0, -1,uWord);
+            else if(hor_dir == -1 && vert_dir == -1 && i > 0 && j > 0) //upper left
+                found = searchWord(board, trie, i - 1, j - 1, -1, -1,uWord);
+            else if(hor_dir == -1 && vert_dir == 1 && i < board.size() - 1 && j > 0) //bottom left
+                found = searchWord(board, trie, i + 1, j - 1, -1, 1,uWord);
+            else if(hor_dir == 1 && vert_dir == -1 && i > 0 && j < board[i].size() - 1) //upper right
+                found = searchWord(board, trie, i - 1, j + 1, 1, -1,uWord);
+
+						if(found) {	
+							boardRes[i][j] = uWord[uWord.length() - 1];
+							return true;
+						}
+        }
+    }
+
+		return false;
+}
+
+int main(int argc, char* argv[]) 
+{
+    Trie trie;
+		std::fstream file("words.txt", std::ios::in);
+		const char empty = ' '; 
+
+		//insert words into trie and set
+		if(file.is_open()) {
+			std::string word;
+
+			while(file >> word) {
+				transform(word.begin(), word.end(), word.begin(), ::toupper);
+
+				words.insert(word);
+				trie.addWord(word);
+			}
+
+			file.close();
+		}
+
+		std::fstream puz_file("puzzle.txt", std::ios::in);
+    std::vector<std::vector<char>> board;
+
+		std::string row;
+
+		//fill game board
+		while(std::getline(puz_file, row)) {
+			std::vector<char> puzzle_row;
+			std::vector<char> res_row;
+			
+			for(unsigned int i = 0; i < row.length(); i+=2) {
+				puzzle_row.push_back(std::toupper(row[i]));
+				res_row.push_back(empty);
+			}
+		
+			board.push_back(puzzle_row);
+			boardRes.push_back(res_row);
+		}
+
+		puz_file.close();
+    printBoard(board);
+
+		std::cout << '\n';
+		std::cout << "Press ENTER to start the game> ";
+		std::cin.get();
+		std::cout << '\n';
+
+    for(unsigned int i = 0; i < board.size(); i++) {
+        for(unsigned int j = 0; j < board[i].size(); j++) {
+            searchWord(board, trie, i, j);
+        }
+    }
+
+		std::cout << '\n';
+		printBoard(boardRes);
+
+    return 0;
+}
+
+void printBoard(const std::vector<std::vector<char>> &board) {
+    for(unsigned int i = 0; i < board.size(); ++i) {
+        for(unsigned int j = 0; j < board[i].size(); ++j) {
+            std::cout << board[i][j] << ' ';
+        }
+        std::cout << '\n';
+    }
+}
+
